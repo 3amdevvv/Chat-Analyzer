7e0533f9669646ac3e7d49499abc2115df2d8c95
3amdevvv <149893506+3amdevvv@users.noreply.github.com>
2024-09-28 22:01:24 +0530

    Update helper.py

--- pythonProject/helper.py
+++ pythonProject/helper.py
@@ -63,7 +63,7 @@ def create_wordcloud(selected_user,df):
 def most_common_word(selected_user,df):
 
 
-    f = open('stop_hinglish.txt','r')
+    f = open('pythonProject/stop_hinglish.txt','r')
     stop_words = f.read()
 
     if selected_user != 'Overall':
